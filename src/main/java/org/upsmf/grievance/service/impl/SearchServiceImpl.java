69652ce6e2e0413e08154b12d58b961322ee768e
rahulbowade <rahul.bowade@tarento.com>
2023-08-11 18:05:52 +0530

    POC code for dashboard

--- src/main/java/org/upsmf/grievance/service/impl/SearchServiceImpl.java
+++ src/main/java/org/upsmf/grievance/service/impl/SearchServiceImpl.java
@@ -1,5 +1,6 @@
 package org.upsmf.grievance.service.impl;
 
+import org.checkerframework.checker.units.qual.C;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.RequestOptions;
@@ -19,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.upsmf.grievance.config.EsConfig;
+import org.upsmf.grievance.constants.Constants;
 import org.upsmf.grievance.dto.SearchRequest;
 import org.upsmf.grievance.enums.RequesterType;
 import org.upsmf.grievance.enums.TicketPriority;
@@ -29,16 +31,19 @@ import org.upsmf.grievance.repository.es.TicketRepository;
 import org.upsmf.grievance.service.SearchService;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class SearchServiceImpl implements SearchService {
 
     @Value("${es.default.page.size}")
     private int defaultPageSize;
+
+    private static final Map<String, Object> assignmentResponse = new HashMap<>();
+    private static final Map<String, Map<String, Object>> resolutionResponse = new HashMap<>();
+    private static final Map<String, Object> departmentNameResponse = new HashMap<>();
+    private static final Map<String, Object> performanceIndicatorsResponse = new HashMap<>();
+    private static final Map<String, Object> finalResponse = new HashMap<>();
     @Autowired
     private TicketRepository esTicketRepository;
     @Autowired
@@ -56,22 +61,59 @@ public class SearchServiceImpl implements SearchService {
     @Override
     public Map<String, Object> dashboardReport(SearchRequest searchRequest) {
         //Create query for search by keyword
-        SearchResponse searchJunkResponse = getDashboardSearchResponse(searchRequest, "isJunk");
-        SearchResponse searchOpenStatusResponse = getDashboardSearchResponse(searchRequest, "openStatus");
-        SearchResponse searchClosedStatusResponse = getDashboardSearchResponse(searchRequest, "closedStatus");
-        SearchResponse searchIsEsclatedResponse = getDashboardSearchResponse(searchRequest, "isEscalated");
-        Map<String, Object> response = new HashMap<>();
-        response.put("isJunk", searchJunkResponse.getHits().getTotalHits().value);
-        response.put("isOpen", searchOpenStatusResponse.getHits().getTotalHits().value);
-        response.put("isClosed", searchClosedStatusResponse.getHits().getTotalHits().value);
-        response.put("isEscalated", searchIsEsclatedResponse.getHits().getTotalHits().value);
+        Map<String, Object> response = null;
+        if (searchRequest.getFilter().get("cc") != null && searchRequest.getFilter().get("cc").equals(Constants.AFFILIATION)) {
+            response = getfinalResponse(searchRequest, Constants.AFFILIATION);
+        } else if (searchRequest.getFilter().get("cc") != null && searchRequest.getFilter().get("cc").equals(Constants.EXAM)) {
+            response = getfinalResponse(searchRequest, Constants.EXAM);
+        } else if (searchRequest.getFilter().get("cc") != null && searchRequest.getFilter().get("cc").equals(Constants.ADMISSION)) {
+            response = getfinalResponse(searchRequest, Constants.ADMISSION);
+        } else if (searchRequest.getFilter().get("cc") != null && searchRequest.getFilter().get("cc").equals(Constants.REGISTRATION)) {
+            response = getfinalResponse(searchRequest, Constants.REGISTRATION);
+        } else if (searchRequest.getFilter().get("cc") != null && searchRequest.getFilter().get("cc").equals(Constants.ASSESSMENT)) {
+            response = getfinalResponse(searchRequest, Constants.ASSESSMENT);
+        } else {
+            response = getfinalResponse(searchRequest, Constants.AFFILIATION);
+        }
         return response;
     }
 
-    private SearchResponse getDashboardSearchResponse(SearchRequest searchRequest, String reportType) {
+    private Map<String, Object> getfinalResponse(SearchRequest searchRequest, Long cc) {
+        SearchResponse searchJunkResponse = getDashboardSearchResponse(searchRequest, "isJunk", cc);
+        SearchResponse searchOpenStatusResponse = getDashboardSearchResponse(searchRequest, "openStatus", cc);
+        SearchResponse searchClosedStatusResponse = getDashboardSearchResponse(searchRequest, "closedStatus", cc);
+        SearchResponse searchIsEsclatedResponse = getDashboardSearchResponse(searchRequest, "isEscalated", cc);
+        SearchResponse searchUnassignedResponse = getDashboardSearchResponse(searchRequest, "openStatus", cc);
+        SearchResponse searchOpenTicketsResponse = getDashboardSearchResponse(searchRequest, "openStatus", cc);
+
+        Map<String, Object> response = new HashMap<>();
+        //response.put(Constants.TOTAL_TICKETS, esTicketRepository.findAllById());
+        response.put(Constants.IS_JUNK, searchJunkResponse.getHits().getTotalHits().value);
+        response.put(Constants.IS_OPEN, searchOpenStatusResponse.getHits().getTotalHits().value);
+        response.put(Constants.IS_CLOSED, searchClosedStatusResponse.getHits().getTotalHits().value);
+        response.put(Constants.IS_ESCALATED, searchIsEsclatedResponse.getHits().getTotalHits().value);
+        assignmentResponse.put(Constants.ASSESSMENT_MATRIX,response);
+        if(cc.equals(Constants.ASSESSMENT)){
+            departmentNameResponse.put(Constants.ASSESSMENT_DEPARTMENT,response);
+        } else if (cc.equals(Constants.AFFILIATION)) {
+            departmentNameResponse.put(Constants.AFFILIATION_NAME,response);
+        } else if (cc.equals(Constants.EXAM)) {
+            departmentNameResponse.put(Constants.EXAM_NAME,response);
+        } else if (cc.equals(Constants.REGISTRATION)) {
+            departmentNameResponse.put(Constants.REGISTRATION_NAME,response);
+        } else if (cc.equals(Constants.ADMISSION)) {
+            departmentNameResponse.put(Constants.ADMISSION_NAME,response);
+        }
+        resolutionResponse.put(Constants.RESOLUTION_MATRIX,departmentNameResponse);
+        //finalResponse.put()
+        return response;
+    }
+
+    private SearchResponse getDashboardSearchResponse(SearchRequest searchRequest, String reportType, Long cc) {
         SearchResponse searchResponse;
         BoolQueryBuilder finalQuery = QueryBuilders.boolQuery();
         finalQuery = getDateRangeQuery(searchRequest, finalQuery);
+        finalQuery = getCCRangeQuery(cc, finalQuery);
         if (reportType.equals("isJunk")) {
             finalQuery = getJunkQuery(true, finalQuery);
         } else if (reportType.equals("openStatus")) {
@@ -259,16 +301,21 @@ public class SearchServiceImpl implements SearchService {
             prioritySearchQuery.must(priorityMatchQuery);
             finalQuery.must(prioritySearchQuery);
         }
-        if (searchRequest.getFilter().get("cc") != null) {
-            MatchQueryBuilder ccMatchQuery = QueryBuilders.matchQuery("assigned_to_id", searchRequest.getFilter().get("cc"));
-            BoolQueryBuilder ccSearchQuery = QueryBuilders.boolQuery();
-            ccSearchQuery.must(ccMatchQuery);
-            finalQuery.must(ccSearchQuery);
-        }
+        getCCRangeQuery((Long) searchRequest.getFilter().get("cc"), finalQuery);
         getDateRangeQuery(searchRequest, finalQuery);
         getStatusQuery((List<String>) searchRequest.getFilter().get("status"), finalQuery);
         getJunkQuery(searchRequest.getIsJunk(), finalQuery);
         getEsclatedTicketsQuery(searchRequest.getIsEscalated(), finalQuery);
+        return finalQuery;
+    }
+
+    private static BoolQueryBuilder getCCRangeQuery(Long cc, BoolQueryBuilder finalQuery) {
+        if (cc != null) {
+            MatchQueryBuilder ccMatchQuery = QueryBuilders.matchQuery("assigned_to_id", cc);
+            BoolQueryBuilder ccSearchQuery = QueryBuilders.boolQuery();
+            ccSearchQuery.must(ccMatchQuery);
+            finalQuery.must(ccSearchQuery);
+        }
         return finalQuery;
     }
 
