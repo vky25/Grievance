49feaad92dd25526e3c428caa06937c7546573b2
rahulbowade <rahul.bowade@tarento.com>
2023-08-21 16:52:24 +0530

    Adding cors filter for All API's

--- src/main/java/org/upsmf/grievance/GrievanceServiceApplication.java
+++ src/main/java/org/upsmf/grievance/GrievanceServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableWebMvc
 @EnableJpaRepositories(basePackages = "org.upsmf.grievance")
 public class GrievanceServiceApplication {
 
