4aecc15a2c4aa93263b3d22e7de21b544aa6d9b7
rahulbowade <rahul.bowade@tarento.com>
2023-08-21 16:57:43 +0530

    logger

--- src/main/java/org/upsmf/grievance/config/CorsConfiguration.java
+++ src/main/java/org/upsmf/grievance/config/CorsConfiguration.java
@@ -1,14 +1,17 @@
 package org.upsmf.grievance.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("Coming inside core mapping method");
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
