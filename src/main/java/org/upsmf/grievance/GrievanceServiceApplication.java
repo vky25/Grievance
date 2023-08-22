d37314e884c92a3f44ae7bb3eb015468b2972e45
rahulbowade <rahul.bowade@tarento.com>
2023-08-22 12:46:23 +0530

    remove pom and other properties

--- src/main/java/org/upsmf/grievance/GrievanceServiceApplication.java
+++ src/main/java/org/upsmf/grievance/GrievanceServiceApplication.java
@@ -2,12 +2,8 @@ package org.upsmf.grievance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,19 +13,4 @@ public class GrievanceServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GrievanceServiceApplication.class, args);
 	}
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				System.out.println("Inside cors filter");
-//				registry.addMapping("/**").allowedOrigins("*")
-//						.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-//						.allowedHeaders("Origin","Content-Type","Accept","Authorization")
-//						.exposedHeaders("Authorization")
-//						.maxAge(3600);
-//			}
-//		};
-//	}
 }
