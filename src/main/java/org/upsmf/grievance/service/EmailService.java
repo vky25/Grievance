3f38226a126f629e11c7838a87ec6efae272327c
Shishir Suman <shishir.suman@tarento.com>
2023-09-04 14:56:53 +0530

    adding email templates

--- src/main/java/org/upsmf/grievance/service/EmailService.java
+++ src/main/java/org/upsmf/grievance/service/EmailService.java
@@ -1,11 +1,14 @@
 package org.upsmf.grievance.service;
 
 import org.upsmf.grievance.model.EmailDetails;
+import org.upsmf.grievance.model.Ticket;
 
 public interface EmailService {
 
     // Method
     // To send a simple email
+    void sendCreateTicketMail(EmailDetails details, Ticket ticket);
+
     void sendSimpleMail(EmailDetails details);
 
     // Method
--- src/main/java/org/upsmf/grievance/service/impl/EmailServiceImpl.java
+++ src/main/java/org/upsmf/grievance/service/impl/EmailServiceImpl.java
@@ -1,20 +1,27 @@
 package org.upsmf.grievance.service.impl;
 
 // Importing required classes
-import java.io.File;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.upsmf.grievance.model.EmailDetails;
+import org.upsmf.grievance.model.Ticket;
 import org.upsmf.grievance.service.EmailService;
+import org.upsmf.grievance.util.DateUtil;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.StringWriter;
 
 // Annotation
 @Service
@@ -24,8 +31,50 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private VelocityEngine velocityEngine;
+
     @Value("${spring.mail.username}")
     private String sender;
+
+    @Override
+    public void sendCreateTicketMail(EmailDetails details, Ticket ticket) {
+        // Try block to check for exceptions
+        try {
+            MimeMessagePreparator preparator = new MimeMessagePreparator() {
+                public void prepare(MimeMessage mimeMessage) throws Exception {
+                    MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+                    message.setTo(details.getRecipient());
+                    message.setSubject(details.getSubject());
+
+                    VelocityContext velocityContext = new VelocityContext();
+                    velocityContext.put("first_name", ticket.getFirstName());
+                    velocityContext.put("id", ticket.getId());
+                    velocityContext.put("created_date", DateUtil.getFormattedDateInString(ticket.getCreatedDate()));
+                    // TODO
+                    velocityContext.put("signature_name", "U.P. State Medical Faculty");
+                    velocityContext.put("phone", "Phone: (0522) 2238846, 2235964, 2235965, 3302100");
+                    velocityContext.put("mobile", "Mobile : +91-8400955546 / +91-9151024463");
+                    velocityContext.put("fax", "Fax : (0522) 2236600");
+                    velocityContext.put("email", "Email:  upmedicalfaculty@upsmfac.org");
+                    velocityContext.put("address", "Address: 5, Sarvpalli, Mall Avenue Road,  Lucknow - 226001 (U.P.) India");
+
+                    StringWriter stringWriter = new StringWriter();
+
+                    velocityEngine.mergeTemplate("templates/raiser-create-ticket.vm", "UTF-8", velocityContext, stringWriter);
+
+                    message.setText(stringWriter.toString(), true);
+                }
+            };
+            // Sending the mail
+            javaMailSender.send(preparator);
+            log.info("create ticket mail Sent Successfully...");
+        }
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            log.error("Error while Sending Mail", e);
+        }
+    }
 
     // Method 1
     // To send a simple email
