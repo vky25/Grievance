43d5f0bbfd36c9ca3d7e8b7f1e84a4892fbcb9e0
rkrahu <rajendra.kishor@tarento.com>
2023-11-29 18:20:10 +0530

    public enum added in RequesterType

--- src/main/java/org/upsmf/grievance/enums/RequesterType.java
+++ src/main/java/org/upsmf/grievance/enums/RequesterType.java
@@ -2,7 +2,7 @@ package org.upsmf.grievance.enums;
 
 public enum RequesterType {
 
-    CANDIDATE(1), INSTITUTE(2), FACULTY(3), OTHERS(4);
+    CANDIDATE(1), INSTITUTE(2), FACULTY(3), OTHERS(4), PUBLIC(5);
 
     private int id;
     RequesterType(int id) {
